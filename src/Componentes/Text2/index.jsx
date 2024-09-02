e84b9af5b185ce99e9d881a1c3a8ca242df27a01
LeoMasago <leo.masago@etec.gov.sp.br>
2024-09-02 19:38:07 -0300

    arrumando tabela

--- src/Componentes/Text2/index.jsx
+++ src/Componentes/Text2/index.jsx
@@ -1,17 +1,17 @@
-function Text2({dadosText}) {
-    return ( 
+function Text2({ dadosText }) {
+    return (
         <>
-        {
-            dadosText.map((pegaText)=> (
-                <div className="personal-info">
-                    <h1>{pegaText.titulo2}</h1>
-                    <p>{pegaText.conteudo2}</p>
-                </div>
-            ))
-        }
+            {
+                dadosText.map((pegaText) => (
+                    <div className="personal-info">
+                        <h1>{pegaText.titulo2}</h1>
+                        <p>{pegaText.conteudo2}</p>
+                    </div>
+                ))
+            }
 
         </>
-     );
+    );
 }
 
 export default Text2;
--- src/Componentes/Text3/index.jsx
+++ src/Componentes/Text3/index.jsx
@@ -1,17 +1,17 @@
-function Text3({dadosText3}) {
-    return ( 
+function Text3({ dadosText }) {
+    return (
         <>
-        {
-            dadosText3.map((pegaText)=> (
-                <div className="personal-info">
-                    <h1>{pegaText.titulo3}</h1>
-                    <p>{pegaText.conteudo3}</p>
-                </div>
-            ))
-        }
+            {
+                dadosText.map((pegaText) => (
+                    <div className="personal-info">
+                        <h1>{pegaText.titulo3}</h1>
+                        <p>{pegaText.conteudo3}</p>
+                    </div>
+                ))
+            }
 
         </>
-     );
+    );
 }
 
 export default Text3;
