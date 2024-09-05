11547e30e9ef9dd62bc8e655b0957604c410f288
LeoMasago <leo.masago@etec.sp.gov.br>
2024-09-04 22:50:28 -0300

    adicionando contato

--- src/Componentes/ContactForm/index.jsx
+++ src/Componentes/ContactForm/index.jsx
@@ -1,12 +1,25 @@
 function ContactForm() {
   return (
     <section id="contact">
-      <h2>Entre em Contato</h2>
       <form>
-        <input type="text" placeholder="Nome" />
-        <input type="email" placeholder="Email" />
-        <textarea placeholder="Mensagem" />
-        <button>Começar</button>
+        <div className="contato">
+          <div className="textos">
+            <p>Nome:</p>
+            <input type="text" placeholder="Nome"/>
+          </div>
+          
+          <div className="textos">
+            <p>Email:</p>
+            <input type="email" placeholder="Email" />
+          </div>
+
+          <div className="mensagem">
+            <p>Mensagem:</p>
+            <textarea placeholder="Mensagem" />
+          </div>
+        </div>
+
+        <button>Enviar</button>
       </form>
     </section>
   );
